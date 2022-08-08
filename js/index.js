41e3cca9d5ea54c24b8d2b3d1bed29932b150398
洪超 <8746124+kin0926@user.noreply.gitee.com>
2022-08-08 16:40:06 +0800

    优化了js代码

--- js/index.js
+++ js/index.js
@@ -6,26 +6,19 @@ for(var i = 0; i< tabs.length; i++){
 }
 
 function showlist(){
+    //排他思想
     for( var i = 0; i<tabs.length; i++){
-        if( tabs[i] === this){
-            tabs[i].className = "active";
-            lists[i].className = "clearfix active";
-        }
-        else {
-            tabs[i].className = "";
-            lists[i].className = "clearfix";
-        }
-        
+        tabs[i].className = "";
+        //这个是下面的商品部分，如果点击的是这个则添加active样式
+        tabs[i] === this ? lists[i].className = "clearfix active" : lists[i].className = "clearfix";
     }
+    this.className = "active";
 }
+
+//时间栏窗口位置固定
 var seckillNav = document.querySelector("#seckillNav");
 window.addEventListener('scroll',function(){
-    var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop || 0;
-    
-    if( scrollTop >= 260){
-        seckillNav.className = "seckill-nav seckill-navfixed"
-    }else {
-        seckillNav.className = "seckill-nav";
-    }
+    var scrollTop = window.scrollY;
+    scrollTop >= 260 ? seckillNav.className = "seckill-nav seckill-navfixed" : seckillNav.className = "seckill-nav";
     console.log(scrollTop);
 })
